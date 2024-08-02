e0e468dfe7099e4d2fd3b62f847c128f5987afcc
FAUZAN AHMAD <110845300+khanfauzan1@users.noreply.github.com>
2024-08-02 15:01:48 +0530

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒMd Kaif Rezwee {year}</p>
+      <p>Copyright ⓒ Md Kaif Rezwee {year}</p>
     </footer>
   );
 }
